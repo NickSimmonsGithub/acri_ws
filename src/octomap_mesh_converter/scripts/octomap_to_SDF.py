f1351d46388f342eab4bb02e8e18ccd5c0a667dc
Nicholas Simmons <simmonsn2018@gmail.com>
2022-08-10 13:30:59 +1000

    temporarily committed current work so that i can make a new branch for georgia's rtabmap installation

--- src/octomap_mesh_converter/scripts/octomap_to_SDF.py
+++ src/octomap_mesh_converter/scripts/octomap_to_SDF.py
@@ -12,7 +12,6 @@ from os.path import exists
 def octomap_to_SDF(filename_in):
 
     # 1: Check input and retrieve file locations.
-
     working_directory = os.getcwd()
     maps_directory = working_directory + "/src/octomap_mesh_converter/maps/"
     simulation_directory = working_directory + "/src/octomap_mesh_converter/simulation/"
@@ -32,16 +31,48 @@ def octomap_to_SDF(filename_in):
             print("Output launch file will be saved at " + launch_directory + " as " + output_name + ".launch")
 
             # 2: Call the roslaunch file which converts filename_in to a mesh and saves it in the 'simulation_directory' folder.
-
             uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
             roslaunch.configure_logging(uuid)
-            cli_args = [launch_directory + "/convert_to_SDF.launch", "package:=" + working_directory + "/src/octomap_mesh_converter", "map_path:=maps/" + filename_in, "model_path:=simulation/" + output_name + ".dae"]
+            cli_args = [launch_directory + "/convert_to_SDF.launch", "package:=" + working_directory + "/src/octomap_mesh_converter", "map_path:=maps/" + filename_in, "model_path:=worlds/" + output_name + ".dae"]
             roslaunch_args = cli_args[1:]
             roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
             parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
             parent.start()
 
-            # 3: Generate a world file and corresponding launch file for the generated mesh.
+            # 3: Generate a world file for the generated mesh.
+            f_world = open(worlds_directory + output_name + '.world', 'x')
+            f_world.write('<?xml version="1.0" ?>\n')
+            f_world.write('<sdf version="1.6">\n')
+            f_world.write('    <world name="' + output_name + '">\n')
+            f_world.write('        <!-- A demo world generated using the octomap_to_SDF python script of acri_ws.-->\n')
+            f_world.write('        <include>\n')
+            f_world.write('            <uri>model://sun</uri>\n')
+            f_world.write('        </include>\n')
+            f_world.write('        <include>\n')
+            f_world.write('            <uri>model://ground_plane</uri>\n')
+            f_world.write('        </include>\n')
+            f_world.write('        <model name="my_mesh">\n')
+            f_world.write('            <pose>0 0 0  0 0 0</pose>\n')
+            f_world.write('            <static>true</static>\n')
+            f_world.write('            <link name="body">\n')
+            f_world.write('                <visual name="visual">\n')
+            f_world.write('                    <geometry>\n')
+            f_world.write('                        <mesh><uri>file://' + output_name + '.dae</uri></mesh>\n')
+            f_world.write('                    </geometry>\n')
+            f_world.write('                </visual>\n')
+            f_world.write('            </link>\n')
+            f_world.write('        </model>\n')
+            f_world.write('    </world>\n')
+            f_world.write('</sdf>\n')
+            
+            # 4: Create a launch file for the generated world file.
+            f_launch = open(launch_directory + output_name + '.launch', 'x')
+            f_launch.write('<launch>\n')
+            f_launch.write('    <include file="$(find gazebo_ros)/launch/empty_world.launch">\n')
+            f_launch.write('        <arg name="world_name" value="$(find octomap_mesh_converter)/worlds/' + output_name + '.world" />\n')
+            f_launch.write('        <arg name="verbose" value="true"/>\n')
+            f_launch.write('    </include>\n')
+            f_launch.write('</launch>\n')
 
         else:
             print("Could not find " + filename_in + " in " + maps_directory)
